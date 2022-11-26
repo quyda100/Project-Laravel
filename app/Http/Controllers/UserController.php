66bc2f8f09f77a422fbd66a6358a7a77c6121890
NhutMinhSSS <nhutminh11102002@gmail.com>
2022-11-26 10:59:50 +0700

    login register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Response;
-use Validator;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -17,22 +19,22 @@ class UserController extends Controller
     public function login(Request $request)
     {
         $messges = [
-        'email' => 'Email phải là 1 địa chỉ hợp lệ'
+        'email.email' => 'Email phải là 1 địa chỉ hợp lệ',
+        'email.required' => 'Email không được bỏ trống',
+        'password.required' => 'Password không được bỏ trống' 
         ];
-       $error = validator($request->all(),[
-            'email' => 'email',
-        ],$messges);
-        
-        if($error->errors()->count()>0)
-            return response()->json(['error' =>$error->errors()->first('email')]);
+       $request->validate([
+            'email' => 'required | email',
+            'password' => 'required'
+       ],$messges);
         $username = $request->email; // bien du lieu tu ajax
-        $password = $request->pass;
-        $Account = DB::table('users')->where('Email', $username)->where('Password', $password)->get();
-        if ($Account->count()) {
-            return 1;
+        $Account = DB::table('users')->where('Email', $username)->first();
+        if (!empty($Account)) {
+             $password =Hash::check($request->password,$Account->Password);
+             return $password;
         }
-        else{
-            return -1;
+         else
+         {   return -1;
         }
     }
     /**
@@ -53,7 +55,33 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        
+        $messges = [
+            'email.required' => 'Email không được bỏ trống',
+            'email.email' => 'Email phải là 1 địa chỉ hợp lệ',
+            'email.unique' => 'Email của bạn đã tồn tại',
+            'password.required' => 'Password không được để trống ',
+            'password.between' => 'Mật khẩu từ 6 đến 20 kí tự',
+            'FullName.required' => 'FullName không được bỏ trống',
+            'Address.required' => 'Address không được bỏ trống',
+            'Phone.required' => 'Phone không được bỏ trống',
+            'Phone.regex' => 'Phone không đúng định dạng',
+            ];
+            $request->validate([
+                'email' => 'required | email| unique:users',
+                'password' => 'required | between:6,20',
+                'FullName' => 'required',
+                'Address' => 'required',
+                'Phone' => 'required  | regex:/(0)([0-9]{9})/'
+
+            ],$messges);
+            $request['password'] = bcrypt($request['password']);
+            $request->all();
+            $check = DB::table('users')->insert(['email' => $request->email,
+                                                 'password' => $request->password,
+                                                 'FullName' => $request->FullName,
+                                                'Address' => $request->Address,
+                                                'Phone' => $request->Phone]);
+            return $check;
     }
 
     /**
