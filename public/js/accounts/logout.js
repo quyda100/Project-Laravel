8d6ec5cb29e27a46b530eac0c4a9a5aad6963cf4
NhutMinhSSS <nhutminh11102002@gmail.com>
2022-11-26 21:20:39 +0700

    complete session

---
+++ public/js/accounts/logout.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var session = $('#session').val();
+    if(session!== undefined){
+       $('#user').append('<li class="nav-item"><a id="logout"><span class="ti-arrow-right"></span></button></li>');
+        $('#logout').click(function(){
+            $.ajax({
+                type: "GET",
+                url: "/logout",
+                success: function (data) {
+                    cuteToast({
+                        title: "Thông báo",
+                        type: "success",
+                        message: "Đăng xuất thành công",
+                        timer: 3000,
+                    })
+                    window.setTimeout(function () {
+                        window.location.href = "/";//chuyen huong trang "route" 
+                    }, 1000);
+                }
+            });
+        });
+    } 
+    console.log(session);
+});
